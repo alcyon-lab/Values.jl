fb28a8e148bc0b34b93457772fdc21f7464e6644
İsmail Tapan <ismltpn@gmail.com>
2024-04-09 00:22:55 +0300

    refactor, update arithmetic operator overrides

--- src/AlcyonValue.jl
+++ src/AlcyonValue.jl
@@ -3,6 +3,9 @@ module AlcyonValue
 using MacroTools
 using TermInterface
 
+export Value
+export substitute
+
 # updated form of
 # https://discourse.julialang.org/t/get-a-symbol-vector-of-all-variable-names-in-an-expr/39391/3
 function symbols(ex)
@@ -24,72 +27,26 @@ function symbols(ex)
     return Set{Symbol}(list)
 end
 
-function Base.:+(a::Symbol, b::Symbol)::Expr
-    return :($a + $b)
-end
-
-# Instead of 
-export Value
 mutable struct Value
     val::Union{Number,Expr,Symbol}
     free::Set{Base.Symbol}
     bound::Set{Base.Symbol}
 end
-
-# For TermInterface
-function isexpr(x::Value)
-    return true
-end
-
-function head(x::Value)
-    return x.val.head
-end
-
-function children(x::Value)
-    return x.val.args[2:end]
-end
-
-function iscall(x::Value)
-    return (x.val.head in [:+, :-, :*, :/, :^])
-end
-
-function operation(x::Value)
-    return x.val.head
-end
-
-function arguments(x::Value)
-    return x.val.args[2:end]
-end
-
-function maketerm(Value, head, children, type=nothing, metadata=nothing)
-    return Value(:($head($children)), Set{Symbol}(), Set{Symbol}())
-end
-
-
-
-function Base.string(v::Value)
-    return string(v.val)
-end
-
 function Value(a::T) where {T<:Number}
     return Value(a, Set{Symbol}([]), Set{Symbol}([]))
 end
-
 function Value(a::Expr)
     return Value(a, symbols(a), Set{Symbol}([]))
 end
-
 function Value(a::Symbol)
     return Value(a, Set{Symbol}([a]), Set{Symbol}([]))
 end
-
 Base.convert(::Type{Value}, x::Number) = Value(x)
 Base.convert(::Type{Value}, x::Expr) = Value(x)
 Base.convert(::Type{Value}, x::Symbol) = Value(x)
 Base.promote_rule(::Type{Value}, ::Type{<:Number}) = Value
 Base.promote_rule(::Type{Value}, ::Type{<:Expr}) = Value
 Base.promote_rule(::Type{Value}, ::Type{<:Symbol}) = Value
-
 function Base.convert(::Type{Number}, x::Value)
     if isa(x.val, Number)
         return x.val
@@ -98,36 +55,7 @@ function Base.convert(::Type{Number}, x::Value)
     end
 end
 
-Base.zero(::Type{Value}) = zero(Number)
-Base.one(::Type{Value}) = one(Number)
-
-function Base.length(x::Value)
-    return 1
-end
-
-function Base.iterate(x::Value, nothing)
-    return nothing
-end
-
-function Base.iterate(x::Value)
-    return (x, nothing)
-end
-
-function Base.isinteger(x::Union{Symbol,Expr})
-    return false
-end
-function Base.isinteger(x::Value)
-    return isinteger(x.val)
-end
-
-function Base.Integer(x::Value)
-    return Integer(x.val)
-end
-
-function Base.transpose(x::Union{Value,Symbol,Expr})
-    return x
-end
-
+# Equality
 function Base.:(==)(b::Value, a::Value)
     return a.val == b.val
 end
@@ -137,20 +65,34 @@ end
 function Base.:(==)(b::Any, a::Value)
     return a.val == b
 end
-function Base.isless(a::Value, b::Value)
-    return isless(a.val, b.val)
+
+# Expr Arithmetics
+function Base.:-(a::Union{Expr,Symbol})::Expr
+    return :(-$a)
 end
-function Base.isless(a::Value, b::Any)
-    return isless(a.val, b)
+function Base.:+(a::Union{Expr,Symbol,Number}, b::Union{Expr,Symbol,Number})::Expr
+    return :($a + $b)
 end
-function Base.isless(a::Any, b::Value)
-    return isless(a, b.val)
+function Base.:-(a::Union{Expr,Symbol,Number}, b::Union{Expr,Symbol,Number})::Expr
+    return :($a - $b)
+end
+function Base.:*(a::Union{Expr,Symbol,Number}, b::Union{Expr,Symbol,Number})::Expr
+    return :($a * $b)
+end
+function Base.:/(a::Union{Expr,Symbol,Number}, b::Union{Expr,Symbol,Number})::Expr
+    return :($a / $b)
+end
+function Base.://(a::Union{Expr,Symbol,Number}, b::Union{Expr,Symbol,Number})::Expr
+    return :($a // $b)
+end
+function Base.:^(a::Union{Expr,Symbol,Number}, b::Union{Expr,Symbol,Number})::Expr
+    return :($a^$b)
 end
 
-function Base.:-(s::Value)::Value
-    return Value(-s.val, s.free, s.bound)
+# Value Arithmetics
+function Base.:-(a::Value)::Value
+    return Value(-a.val, a.free, a.bound)
 end
-
 function Base.:+(a::Value, b::Union{Number,Expr,Symbol})::Value
     return Value(a.val + b, union(a.free, Set{Symbol}(symbols(b))), a.bound)
 end
@@ -166,8 +108,9 @@ end
 function Base.://(a::Value, b::Union{Number,Expr,Symbol})::Value
     return Value(a.val // b, union(a.free, Set{Symbol}(symbols(b))), a.bound)
 end
-
-
+function Base.:^(a::Value, b::Union{Number,Expr,Symbol})::Value
+    return Value(a.val^b, union(a.free, Set{Symbol}(symbols(b))), a.bound)
+end
 
 function Base.:+(b::Union{Number,Expr,Symbol}, a::Value)::Value
     return Value(b + a.val, union(a.free, Set{Symbol}(symbols(b))), a.bound)
@@ -184,191 +127,48 @@ end
 function Base.://(b::Union{Number,Expr,Symbol}, a::Value)::Value
     return Value(b // a.val, union(a.free, Set{Symbol}(symbols(b))), a.bound)
 end
-
-
-function Base.:^(v::Value, e::Value)::Value
-    return Value((v.val^e.val).val, union(v.free, Set{Symbol}(symbols(e))), v.bound)
-end
-function Base.:^(v::Value, e::Number)::Value
-    return Value((v.val^e).val, v.free, v.bound)
-end
-function Base.:^(v::Value, e::Expr)::Value
-    return Value((v.val^e).val, union(v.free, Set{Symbol}(symbols(e))), v.bound)
+function Base.:^(b::Union{Number,Expr,Symbol}, a::Value)::Value
+    return Value(b^a.val, union(a.free, Set{Symbol}(symbols(b))), a.bound)
 end
 
-function Base.:^(v::Symbol, e::Value)::Value
-    return Value(:($v^$e), union(Set{Symbol}(symbols(v)), Set{Symbol}(symbols(e))), union(Set{Symbol}(symbols(v)), Set{Symbol}(symbols(e))))
-end
-function Base.:^(v::Symbol, e::Number)::Value
-    return Value(:($v^$e), union(Set{Symbol}(symbols(v)), Set{Symbol}(symbols(e))), union(Set{Symbol}(symbols(v)), Set{Symbol}(symbols(e))))
-end
-function Base.:^(v::Symbol, e::Expr)::Value
-    return Value(:($v^$e), union(Set{Symbol}(symbols(v)), Set{Symbol}(symbols(e))), union(Set{Symbol}(symbols(v)), Set{Symbol}(symbols(e))))
-end
-
-# TODO: not sure about taking the union of bounds
+# TODO: still not sure about taking the union of bounds
 function Base.:+(a::Value, b::Value)::Value
-    return Value(a.val + b.val, union(a.free, Set{Symbol}(symbols(b))), union(a.bound, b.bound))
+    return Value(a.val + b.val, union(a.free, b.free), union(a.bound, b.bound))
 end
 function Base.:-(a::Value, b::Value)::Value
-    return Value(a.val - b.val, union(a.free, Set{Symbol}(symbols(b))), union(a.bound, b.bound))
+    return Value(a.val - b.val, union(a.free, b.free), union(a.bound, b.bound))
 end
 function Base.:*(a::Value, b::Value)::Value
-    return Value(a.val * b.val, union(a.free, Set{Symbol}(symbols(b))), union(a.bound, b.bound))
+    return Value(a.val * b.val, union(a.free, b.free), union(a.bound, b.bound))
 end
 function Base.:/(a::Value, b::Value)::Value
-    return Value(a.val / b.val, union(a.free, Set{Symbol}(symbols(b))), union(a.bound, b.bound))
+    return Value(a.val / b.val, union(a.free, b.free), union(a.bound, b.bound))
 end
 function Base.://(a::Value, b::Value)::Value
-    return Value(a.val // b.val, union(a.free, Set{Symbol}(symbols(b))), union(a.bound, b.bound))
+    return Value(a.val // b.val, union(a.free, b.free), union(a.bound, b.bound))
+end
+function Base.:^(a::Value, b::Value)::Value
+    return Value(a.val^b.val, union(a.free, b.free), union(a.bound, b.bound))
 end
 
+# Substitution
 
-function Base.:-(s::Union{Symbol,Expr})::Expr
-    return :(-$s)
-end
-
-function Base.:+(ex::Expr, n::Number)::Expr
-    return :($ex + $n)
-end
-function Base.:+(ex::Expr, s::Symbol)::Expr
-    return :($ex + $s)
-end
-function Base.:+(s::Symbol, n::Number)::Expr
-    return :($s + $n)
-end
-
-
-
-function Base.:+(n::Number, ex::Expr)::Expr
-    return :($n + $ex)
-end
-function Base.:+(s::Symbol, ex::Expr)::Expr
-    return :($s + $ex)
-end
-function Base.:+(n::Number, s::Symbol)::Expr
-    return :($n + $s)
-end
-
-function Base.:-(ex::Expr, n::Number)::Expr
-    return :($ex - $n)
-end
-
-function Base.:*(ex::Expr, n::Number)::Expr
-    if n == 0
-        return :(0)
-    elseif n == 1
-        return :($ex)
-    else
-        return :($ex * $n)
-    end
-
-end
-function Base.:*(n::Number, ex::Expr)::Expr
-    if n == 0
-        return 0
-    elseif n == 1
-        return :($ex)
-    else
-        return :($n * $ex)
-    end
-end
-
-function Base.:*(s::Symbol, n::Number)::Expr
-
-    if n == 0
-        return 0
-    else
-        return :($s * $n)
-    end
-end
-function Base.:*(n::Number, s::Symbol)::Expr
-
-    if n == 0
-        return 0
-    else
-        return :($n * $s)
-    end
-end
-
-function Base.:*(ex::Expr, s::Symbol)::Expr
-
-    return :($ex * $s)
-end
-function Base.:*(s::Symbol, ex::Expr)::Expr
-
-    return :($s * $ex)
-end
-
-function Base.:*(s1::Symbol, s2::Symbol)::Expr
-
-    return :($s1 * $s2)
-end
-
-function Base.:/(ex::Expr, n::Number)::Expr
-    return :($ex / $n)
-end
-
-function Base.://(ex::Expr, n::Number)::Expr
-    return :($ex // $n)
-end
-
-function Base.:+(ex1::Expr, ex2::Expr)::Expr
-    return :($ex1 + $ex2)
-end
-
-function Base.:-(ex1::Expr, ex2::Expr)::Expr
-    if ex1 == ex2
-        return 0
-    else
-        return :($ex1 - $ex2)
-    end
-end
-
-function Base.:*(ex1::Expr, ex2::Expr)::Expr
-
-    return :($ex1 * $ex2)
-end
-
-function Base.:/(ex1::Expr, ex2::Expr)::Expr
-    return :($ex1 / $ex2)
-end
-
-function Base.://(ex1::Expr, ex2::Expr)::Expr
-    return :($ex1 // $ex2)
-end
-
-
-
-function Base.:-(ex1::Symbol, ex2::Number)::Expr
-    return :($ex1 - $ex2)
-end
-function Base.:-(ex1::Number, ex2::Symbol)::Expr
-    return :($ex1 - $ex2)
-end
-
-function Base.:-(ex1::Expr, ex2::Symbol)::Expr
-    return :($ex1 - $ex2)
-end
-function Base.:-(ex1::Symbol, ex2::Expr)::Expr
-    return :($ex1 - $ex2)
-end
-
-function substitute(e::Value, pair)
+function substitute(e::Value, pair::Pair)
     return substitute(e.val, pair)
 end
 
-function substitute(e::Symbol, pair)
+function substitute(e::Symbol, pair::Pair)
     if e == pair[1]
         return pair[2]
     end
-end
-
-function substitute(e::Number, pair)
     return e
 end
 
-function substitute(e::Expr, pair)
+function substitute(e::Number, ::Pair)
+    return e
+end
+
+function substitute(e::Expr, pair::Pair)
     MacroTools.postwalk(e) do s
         if s == pair.first
             return pair.second
